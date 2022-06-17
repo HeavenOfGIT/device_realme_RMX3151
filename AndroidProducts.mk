b407f173637ddcfb99d237bbe316fc909059e3f0
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-06-17 22:16:13 +0700

    RMX3151: it turns out that RMX3151 and RMX3286 runs on same firmware

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX3151.mk
+    $(LOCAL_DIR)/komodo_RMX3151.mk
+
+COMMON_LUNCH_CHOICES := \
+        komodo_RMX3151-eng \
+        komodo_RMX3151-user \
+        komodo_RMX3151-userdebug
