3ee74ead2ab59000aeb655ff5ce4393be068468d
Rayhan77 <96913250+RayhanDz@users.noreply.github.com>
2022-06-19 05:41:58 +0700

    RMX3286 > RMX3151

--- releasetools.py
+++ releasetools.py
@@ -18,11 +18,17 @@ import common
 import re
 
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
-  return
+     OTA_InstallEnd(info)
+     return
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+     OTA_InstallEnd(info)
+     return
+
+def FullOTA_InstallBegin(info):
+  data = info.input_zip.read("RADIO/dynamic-remove-oppo")
+  common.ZipWriteStr(info.output_zip, "dynamic-remove-oppo", data)
+  info.script.AppendExtra('update_dynamic_partitions(package_extract_file("dynamic-remove-oppo"));')
   return
 
 def AddImage(info, basename, dest):
@@ -33,5 +39,6 @@ def AddImage(info, basename, dest):
 
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
+  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   return
